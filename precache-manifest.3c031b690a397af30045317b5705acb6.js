04b662fefed32f3ab4be6cf9f3406f3790a8dcaf
Ranjeet Kumar <ranjeetkumar@procreate.co.in>
2020-12-07 22:40:40 +0530

    Updates

---
+++ precache-manifest.3c031b690a397af30045317b5705acb6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72557e8774aa5cd87cc7aa0d6fca477",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/index.html"
+  },
+  {
+    "revision": "2f261de33750492ace0e",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/css/2.da82b403.chunk.css"
+  },
+  {
+    "revision": "e251c1d419de624d1fdd",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/css/main.9e2fea90.chunk.css"
+  },
+  {
+    "revision": "2f261de33750492ace0e",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/js/2.09eadb7a.chunk.js"
+  },
+  {
+    "revision": "e251c1d419de624d1fdd",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/js/main.cff5399f.chunk.js"
+  },
+  {
+    "revision": "2aff51444f6ee5981283",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/js/runtime~main.a4de3b18.js"
+  },
+  {
+    "revision": "2ab67eec1a3ab14d3cc6fc3eaa072181",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/media/ag-index-background.2ab67eec.png"
+  },
+  {
+    "revision": "8504fae6a8575e0a9786f1c67bd05e69",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/media/icons.8504fae6.eot"
+  },
+  {
+    "revision": "bc005709a21d5a2af39b0744935ace95",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/media/icons.bc005709.woff"
+  },
+  {
+    "revision": "efe053d475acd04c59283fc07f718371",
+    "url": "/Ranjeetkumars/online_talk_with_ranjeet.git/static/media/icons.efe053d4.ttf"
+  }
+]);
